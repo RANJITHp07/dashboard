e9ef5161cd6616baafa601562c5c66688332422f
ranjith <ranjithp27122003@gmail.com>
2024-03-26 21:45:57 +0530

    refactor: added comments for clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import NavBar from "./Components/navBar";
 import SideBar from "./Components/sideBar";
 import dynamic from 'next/dynamic'
 
-// work only without SSR
+// work only without SSR due to window usage
 const Chart = dynamic(
   () => import('./Components/Chart'),
   { ssr: false }
@@ -17,13 +17,16 @@ export default function Home() {
     <div className="bg-[#F2F4F7] min-h-screen">
       <div className="flex">
         <div className="">
+          {/* side bar of the page */}
          <SideBar/> 
         </div>
         <div className="w-full">
          <NavBar/>
          <div className=" py-7 md:py-12 px-[18px] xl:px-[32px]">
           <Header/>
+           {/* chart to show the users*/}
           <Chart/>
+          {/*table to list out*/}
           <Table/>
          </div>
         </div>
